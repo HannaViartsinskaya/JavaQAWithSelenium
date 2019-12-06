d5fd860f6bec5e9333fdf1f3221a01c046df76b4
HannaViartsinskaya <vertinskaya95@gmail.com>
2019-12-06 12:24:59 +0100

    edit project

--- allTasksWithoutCleanCode/src/main/java/com/collections/optionaltask/Main.java
+++ allTasksWithoutCleanCode/src/main/java/com/collections/optionaltask/Main.java
@@ -1,33 +1,46 @@
 package com.collections.optionaltask;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 
         //1. Read strings from file, reverse them and write to file.
 
-//        Scanner sc=new Scanner((new File("./src/main/resources/read1.txt")));
-//        Stack<String> strings=new Stack<String>();
-//        while(sc.hasNextLine())
-//        {
-//            strings.push(sc.nextLine());
-//        }
-//        sc.close();
-//
-//        FileWriter nameFile = new FileWriter("./src/main/resources/write1.txt");
-//        while(!strings.empty()) {
-//            nameFile.write(strings.pop()+"\n");
-//        }
-//        nameFile.close();
+        Scanner sc= null;
+        try {
+            sc = new Scanner((new File("./src/main/resources/read1.txt")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Stack<String> strings=new Stack<String>();
+        while(sc.hasNextLine())
+        {
+            strings.push(sc.nextLine());
+        }
+        sc.close();
+
+        FileWriter nameFile = null;
+        try {
+            nameFile = new FileWriter("./src/main/resources/write1.txt");
+
+        while(!strings.empty()) {
+            nameFile.write(strings.pop()+"\n");
+        }
+        nameFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //Second task. Enter a number, put its numbers on the stack. Print a number whose numbers go in the reverse order.
 //        Scanner in = new Scanner(System.in);
 //        System.out.print("Input a number: ");
 //        int number = in.nextInt();
-//        sc.close();
 //        int numberOfDigits = (int)(Math.log10(number)+1);
 //        Stack<Integer> stackForNumber=new Stack<Integer>();
 //        for (int i=numberOfDigits-1;i>=0;i--)
@@ -45,19 +58,28 @@ public class Main {
 
 
         //7. Задана строка, состоящая из символов «(», «)», «[», «]», «{», «}». Проверить правильность расстановки скобок. Использовать стек.
-//       String usersString="";
-//        Scanner sc=new Scanner((new File("./src/main/resources/7exercise.txt")));
-//    while(sc.hasNextLine())
-//    {
-//        usersString+=sc.nextLine();
-//    }
-//       sc.close();
+//        String usersString = "";
+//        Scanner sc = null;
+//        try {
+//            sc = new Scanner((new File("./src/main/resources/7exercise.txt")));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        while (sc.hasNextLine()) {
+//            usersString += sc.nextLine();
+//        }
+//        sc.close();
 //        System.out.println(usersString);
-//        System.out.println(checkBrackets(usersString));
+//        System.out.println("Все скобки стоят правильно? "+checkBrackets(usersString));
 
         //6. Ввести строки из файла, записать в список ArrayList. Выполнить сортировку строк, используя метод sort() из класса Collections.
 //        List<String> usersString=new ArrayList<String>();
-//        Scanner sc=new Scanner((new File("./src/main/resources/6exercise.txt")));
+//        Scanner sc= null;
+//        try {
+//            sc = new Scanner((new File("./src/main/resources/6exercise.txt")));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 //        while(sc.hasNextLine())
 //        {
 //            usersString.add(sc.nextLine());
@@ -67,7 +89,12 @@ public class Main {
 
         //4. Занести стихотворение в список. Провести сортировку по возрастанию длин строк.
 //        List<String> strings=new ArrayList<String>();
-//        Scanner sc=new Scanner((new File("./src/main/resources/4exercise.txt")));
+//        Scanner sc= null;
+//        try {
+//            sc = new Scanner((new File("./src/main/resources/4exercise.txt")));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 //        while(sc.hasNextLine())
 //        {
 //            strings.add(sc.nextLine());
@@ -78,23 +105,28 @@ public class Main {
 
         //8  Задан файл с текстом на английском языке. Выделить все различные слова. Слова, отличающиеся только регистром букв,
         // считать одинаковыми. Использовать класс HashSet.
-        List<String> strings=new ArrayList<String>();
-        Set<String> setWithDifferentWords=new HashSet<String>();
-        Scanner sc=new Scanner((new File("./src/main/resources/8exercise.txt")));
-        while(sc.hasNextLine())
-      {
-            strings.add(sc.nextLine());
-      }
-
-        for(int i=0;i<strings.size();i++)
-        {
-            String words[]=strings.get(i).split(" ");
-            for(String word : words)
-            {
-                setWithDifferentWords.add(word.toLowerCase());
-            }
-        }
-        System.out.println(setWithDifferentWords);
+//        List<String> strings=new ArrayList<String>();
+//        Set<String> setWithDifferentWords=new HashSet<String>();
+//        Scanner sc= null;
+//        try {
+//            sc = new Scanner((new File("./src/main/resources/8exercise.txt")));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        while(sc.hasNextLine())
+//      {
+//            strings.add(sc.nextLine());
+//      }
+//
+//        for(int i=0;i<strings.size();i++)
+//        {
+//            String words[]=strings.get(i).split(" ");
+//            for(String word : words)
+//            {
+//                setWithDifferentWords.add(word.toLowerCase());
+//            }
+//        }
+//        System.out.println(setWithDifferentWords);
 
     }
 
@@ -107,7 +139,7 @@ public class Main {
     static boolean checkBrackets(String str) {
         if (str.isEmpty())
             return true;
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < str.length(); i++) {
             char current = str.charAt(i);
             if (current == '{' || current == '(' || current == '[') {
