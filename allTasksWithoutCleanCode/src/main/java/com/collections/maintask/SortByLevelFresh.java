ce3db37f91ca6b795fcef211055d38a38905b06a
HannaViartsinskaya <vertinskaya95@gmail.com>
2019-12-08 11:56:39 +0100

    edit collections

--- allTasksWithoutCleanCode/src/main/java/com/collections/maintask/SortByLevelFresh.java
+++
@@ -1,9 +0,0 @@
-package com.collections.maintask;
-
-import java.util.Comparator;
-
-public class SortByLevelFresh implements Comparator<Flowers> {
-    public int compare(Flowers a, Flowers b) {
-        return a.getLevelFresh() - b.getLevelFresh();
-    }
-}
